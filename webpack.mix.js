b77b4f771fe5e633303e676013ef15217b9522e4
Cristobal Berrios <elcriss96@gmail.com>
2018-09-30 01:41:26 -0300

    se agregan los estilos de vuetify

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,4 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .styles('node_modules/vuetify/dist/vuetify.min.css','public/assets/css/vuetify.css');
