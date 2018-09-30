7850ca6ec9d211be7821633b9812cc3726b8ba1a
Cristobal Berrios <elcriss96@gmail.com>
2018-09-30 04:02:30 -0300

    vista de los usuarios

---
+++ resources/assets/js/components/usuarios/UsuariosComponent.vue
@@ -0,0 +1,18 @@
+<template>
+<section>
+  <v-flex xs12 md8 lg8>
+    <v-flex offset-md8 offset-lg8 xs12>
+      <v-btn block dark>Agregar Usuario</v-btn>
+    </v-flex>
+    <tabla-usuarios></tabla-usuarios>
+  </v-flex>
+</section>
+</template>
+
+<script>
+import TablaUsuarios from './TablaUsuarios'
+export default {
+  components: {TablaUsuarios}
+}
+</script>
+
