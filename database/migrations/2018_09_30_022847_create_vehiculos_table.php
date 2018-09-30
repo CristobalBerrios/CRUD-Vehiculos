be1ad473dc72508a33ab17844b3557faad1e4594
Cristobal Berrios <elcriss96@gmail.com>
2018-09-29 23:49:44 -0300

    migracion para crear la tabla de vehiculos

---
+++ database/migrations/2018_09_30_022847_create_vehiculos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVehiculosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehiculos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('marca');
+            $table->string('modelo');
+            $table->integer('year');
+            $table->integer('precio');
+            $table->integer('id_usuario')->unsigned();
+			$table->foreign('id_usuario')->references('id')->on('usuarios')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehiculos');
+    }
+}
