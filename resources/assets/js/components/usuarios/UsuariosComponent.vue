95ec2afd4c62deb3520cbc99741c45b456a34873
Cristobal Berrios <elcriss96@gmail.com>
2018-10-01 19:00:02 -0300

    se agrega la funcion para agregar un usuario

--- resources/assets/js/components/usuarios/UsuariosComponent.vue
+++ resources/assets/js/components/usuarios/UsuariosComponent.vue
@@ -2,17 +2,44 @@
 <section>
   <v-flex xs12 md8 lg8>
     <v-flex offset-md8 offset-lg8 xs12>
-      <v-btn block dark>Agregar Usuario</v-btn>
+      <v-btn block dark @click="showForm">Agregar Usuario</v-btn>
     </v-flex>
-    <tabla-usuarios></tabla-usuarios>
+    <tabla-usuarios @editarUsuario="showForm"></tabla-usuarios>
   </v-flex>
+  <form-usuario 
+    :usuario="usuario"
+    :dialog="dialog"
+    @nuevoUsuario="nuevoUsuario"
+    @actualizarUsuario="actualizarUsuario"
+    @closeDialog="dialog = false">
+  </form-usuario>
 </section>
 </template>
 
 <script>
 import TablaUsuarios from './TablaUsuarios'
+import FormUsuario from './FormUsuario'
 export default {
-  components: {TablaUsuarios}
+  data () {
+    return {
+      dialog: false,
+      usuario: {}
+    }
+  },
+  components: {TablaUsuarios, FormUsuario},
+  methods: {
+    showForm (usuario) {
+      if (usuario) this.usuario = usuario
+      else this.usuario = {}
+      this.dialog = true
+    },
+    nuevoUsuario (usuario) {
+      this.$emit('nuevoUsuario', usuario)
+    },
+    actualizarUsuario (usuario) {
+      this.$emit('actualizarUsuario', usuario)
+    }
+  }
 }
 </script>
 
