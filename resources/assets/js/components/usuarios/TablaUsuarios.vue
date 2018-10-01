967d005248abddf7df8356a93a31036bfdeb94a8
Cristobal Berrios <elcriss96@gmail.com>
2018-10-01 18:59:22 -0300

    se agrega la funcion para eliminar un usuario

--- resources/assets/js/components/usuarios/TablaUsuarios.vue
+++ resources/assets/js/components/usuarios/TablaUsuarios.vue
@@ -13,12 +13,23 @@
           <v-btn @click="editarUsuario(props.item)" icon>
             <v-icon color="blue">edit</v-icon>
           </v-btn>
-          <v-btn icon>
+          <v-btn @click="index = props.index, dialog = true" icon>
             <v-icon color="red">delete</v-icon>
           </v-btn>
         </td>
       </template>
     </v-data-table>
+    <v-dialog v-model="dialog" persistent max-width="290">
+      <v-card>
+        <v-card-title class="headline">Eliminar Usuario</v-card-title>
+        <v-card-text>¿Esta seguro de eliminar el usuario ?</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" flat @click.native="eliminarUsuario()">Confirmar</v-btn>
+          <v-btn color="green darken-1" flat @click.native="dialog = false">Cancelar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </section>
 </template>
 
@@ -33,10 +44,15 @@ export default {
         { text: '',  align: 'left' }
       ],
       usuarios: [],
-      usuarioEdit: {}
+      usuarioEdit: {},
+      index: 0,
+      dialog: false
     }
   },
   methods: {
+    nuevoUsuario (usuario) {
+      this.usuarios.push(usuario)
+    },
     editarUsuario (usuario) {
       this.$emit('editarUsuario',JSON.parse(JSON.stringify(usuario)))
       this.usuarioEdit = usuario
@@ -45,9 +61,17 @@ export default {
       this.usuarioEdit.nombre = usuario.nombre
       this.usuarioEdit.apellidos = usuario.apellidos
       this.usuarioEdit.correo = usuario.correo
+    },
+    eliminarUsuario () {
+      let id = this.usuarios[this.index].id
+      axios.delete(`/api/usuarios/${id}`).then(response => {
+        this.dialog = false
+        this.usuarios.splice(this.index, 1)
+      })
     }
   },
   created () {
+    this.$parent.$on('nuevoUsuario', this.nuevoUsuario)
     this.$parent.$on('actualizarUsuario', this.actualizarUsuario)
 
     axios.get('/api/usuarios').then(response => {
