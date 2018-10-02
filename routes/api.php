d72397feb0709fc837a875f79e418983ef0f2790
Cristobal Berrios <elcriss96@gmail.com>
2018-10-02 01:30:29 -0300

    se agrega endpoint para los vehiculos

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('usuarios', 'UsuarioController');
+Route::apiResource('vehiculos', 'VehiculoController');
