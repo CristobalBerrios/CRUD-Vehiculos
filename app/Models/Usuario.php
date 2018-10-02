b34c615650a2eb183c9d120cc4d9f329bccaf47e
Cristobal Berrios <elcriss96@gmail.com>
2018-10-02 01:31:42 -0300

    se agrega un nuevo atributo, el nombre_completo

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -12,8 +12,21 @@ class Usuario extends Model
   protected $table = 'usuarios';
   protected $fillable = ['nombre', 'apellidos', 'correo'];
   protected $dates = ['deleted_at'];
+  protected $appends = ['nombre_completo'];
 
   public function vehiculos() {
     return $this->hasMany('App\Models\Vehiculo');
   }
+
+  public function getNombreCompletoAttribute () {
+    return $this->attributes['nombre']. ' '. $this->attributes['apellidos'];
+  }
+
+  protected static function boot() { 
+    parent::boot(); 
+
+    static::deleting(function($usuario) { 
+     $usuario->vehiculos()->delete(); 
+    }); 
+} 
 }
